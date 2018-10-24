2676c08ee05420408ef67dc12a88c1f2f5d536c1
Lorna <lornarmac@gmail.com>
2018-10-24 09:13:19 +0100

    unbinding everything before draw loop (core profile with vao but still binding buffer )

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -119,6 +119,11 @@ static unsigned int CreateShader(const std::string& vertexShader, const std::str
 int main(void)
 {
 	GLFWwindow* window;
+	float red = 0.0f;
+	float green = 0.0f;
+	float blue = 0.0f;
+	float alpha = 0.0f;
+	float increment = 0.0001f;
 
 	/* Initialize the library */
 	if (!glfwInit())
@@ -149,10 +154,9 @@ int main(void)
 	}
 	float positions[] = {
 		-0.5f, -0.5f,		// 0 (vertex 0, position attribute only)
-		0.5f, -0.5f,		// 1	
-		0.5f, 0.5f,			// 2
-		-0.5f, 0.5f,		// 3	
-
+		 0.5f, -0.5f,		// 1	
+		 0.5f,  0.5f,		// 2
+		-0.5f,  0.5f,		// 3	
 	};
 
 	unsigned int indices[] = {		// could use char / unsigned short, BUT it MUST be an unsigned type
@@ -160,50 +164,41 @@ int main(void)
 		2, 3, 0
 	};
 
+	// create vertex array:
 	unsigned int vao;
 	GLCall(glGenVertexArrays(1, &vao));
 	GLCall(glBindVertexArray(vao));
 
+	// create vertex buffer:
 	unsigned int bufferid;
 	GLCall(glGenBuffers(1, &bufferid));	// get a buffer id
 	GLCall(glBindBuffer(GL_ARRAY_BUFFER, bufferid));		// select the buffer
 	GLCall(glBufferData(GL_ARRAY_BUFFER, 2 * 4 * sizeof(float), positions, GL_STATIC_DRAW));	// assign data to the buffer
 
+	// specify layout of the vertex buffer (so opengl knows how to interpret the data)
 	GLCall(glEnableVertexAttribArray(0));
-	// tell opengl the layout of the buffer:
 	GLCall(glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0));	// **2nd param of 2 indicates that the data is a vec2
 
-	unsigned int ibo;	// index buffer object
+	// create index buffer:
+	unsigned int ibo;
 	GLCall(glGenBuffers(1, &ibo));
 	GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo));		// select the buffer
-	GLCall(glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(unsigned int), indices, GL_STATIC_DRAW));
+	GLCall(glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(unsigned int), indices, GL_STATIC_DRAW)); // assign data to index buffer
 
+	// create shader:
 	ShaderProgramSource source = ParseShader("res/shaders/basic.shader");
-	std::cout << "VERTEX shader" << std::endl;
-	std::cout << source.VertexSource << std::endl;
-	std::cout << "FRAGMENT shader" << std::endl;
-	std::cout << source.FragmentSource << std::endl;
-
 	unsigned int shader = CreateShader(source.VertexSource, source.FragmentSource);
 	GLCall(glUseProgram(shader));
 
+	// set up the uniform (variable) used by the shader
 	GLCall(int location = glGetUniformLocation(shader, "u_Colour"));
 	ASSERT(location != -1);	// note -1 means the uniform isn't used in the program, we can assert because we know it is
-
 	GLCall(glUniform4f(location, 0.2f, 0.3f, 0.8f, 1.0f));
 
-	float red = 0.0f;
-	float green = 0.0f;
-	float blue = 0.0f;
-	float alpha = 0.0f;
-
-	float increment = 0.0001f;
-
 	/* unbind everything - we're doing this to make clear the steps needed each time we do a draw below */
-	//glUseProgram(0);
-	//glBindBuffer(GL_ARRAY_BUFFER, 0);
-	//glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
-	//glBindVertexArray(0);
+	GLCall(glUseProgram(0));
+	GLCall(glBindBuffer(GL_ARRAY_BUFFER, 0));
+	GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0));
 
 	/* Loop until the user closes the window */
 	while (!glfwWindowShouldClose(window))
@@ -212,17 +207,16 @@ int main(void)
 		glClear(GL_COLOR_BUFFER_BIT);
 
 		/* Do necessary binding before we draw */
-		//GLCall(glUseProgram(shader));
+		GLCall(glUseProgram(shader));
 		GLCall(glUniform4f(location, red, green, blue, 1.0f));
 
-		//GLCall(glBindBuffer(GL_ARRAY_BUFFER, bufferid));
-		//GLCall(glBindVertexArray(vao));
+		GLCall(glBindBuffer(GL_ARRAY_BUFFER, bufferid));
 
 		// tell opengl the layout of the buffer:
-		//GLCall(glEnableVertexAttribArray(0));
+		GLCall(glEnableVertexAttribArray(0));
 		// bind array buffer to layout
-		//GLCall(glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0));	// **2nd param of 2 indicates that the data is a vec2		
-		//GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo));
+		GLCall(glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0));	// **2nd param of 2 indicates that the data is a vec2		
+		GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo));
 
 		GLCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr));	// can use nullptr because index buffer already bound
 
